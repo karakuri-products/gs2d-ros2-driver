7dd277d28053b5561493e5c94e6dc21ec502d377
Hideyuki Takei <takehide22@gmail.com>
2020-08-28 18:50:46 +0900

    Support TorqueEnable

--- gs2d_ros2_driver/gs2d_ros2_driver/gs2d_ros2_driver.py
+++ gs2d_ros2_driver/gs2d_ros2_driver/gs2d_ros2_driver.py
@@ -1,23 +1,52 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32
+from gs2d_ros2_driver_msg.msg import TargetPosition, TorqueEnable
 
 
 class Gs2dRos2Driver(Node):
     def __init__(self):
         super().__init__('gs2d_ros2_driver')
 
-        self.pub_current_position = self.create_subscription(Float32, '/gs2d_ros2_driver/current_position')
-        self.sub_target_position = self.create_subscription(Float32, '/gs2d_ros2_driver/target_position')
+        self.get_logger().info('Yes! initializing...')
+
+        # self.pub_current_position = self.create_publisher(Float32, '/gs2d_ros2_driver/current_position', 10)
+
+        self.sub_target_position = self.create_subscription(
+            TargetPosition,
+            '/gs2d_ros2_driver/target_position',
+            self.target_position_callback,
+            10
+        )
+
+        self.sub_torque_enable = self.create_subscription(
+            TorqueEnable,
+            '/gs2d_ros2_driver/torque_enable',
+            self.torque_enable_callback,
+            10
+        )
+
+        # timer_period = 0.5
+        # self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def target_position_callback(self, msg):
+        self.get_logger().info('subscribe target position: {} (id: {})'.format(msg.data, msg.servo_id))
+
+    def torque_enable_callback(self, msg):
+        self.get_logger().info('subscribe torque enable: {} (id: {})'.format(msg.data, msg.servo_id))
+
+    # def timer_callback(self):
+    #     self.get_logger().info('Yes! initializing...')
 
 
 def main(args=None):
     rclpy.init(args=args)
     node = Gs2dRos2Driver()
+
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
         pass
+
     node.destroy_node()
     rclpy.shutdown()
 
--- gs2d_ros2_driver/setup.py
+++ gs2d_ros2_driver/setup.py
@@ -1,30 +1,20 @@
-import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
 package_name = 'gs2d_ros2_driver'
-
-
-def read_requirements():
-    """Parse requirements from requirements.txt."""
-    reqs_path = os.path.join('.', 'requirements.txt')
-    with open(reqs_path, 'r') as f:
-        requirements = [line.rstrip() for line in f]
-    return requirements
 
 
 setup(
     name=package_name,
     version='0.0.1',
     packages=[package_name],
+    py_modules=['gs2d_ros2_driver.gs2d_ros2_driver'],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=read_requirements(),
-    zip_safe=True,
+    install_requires=['setuptools', 'gs2d'],
+    zip_safe=False,
     description='ROS2 driver for gs2d',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
     author='Karakuri Products',
     author_email='gs2d@krkrpro.com',
     license='Apache 2.0',
